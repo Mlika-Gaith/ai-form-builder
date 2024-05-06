0b2583852f0bbc0f6a4cfc10a370c05072a552b5
ghaith_mlika <mlika.gaith@gmail.com>
2024-05-06 23:24:00 +0100

    Make navbar responsive.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 "use client";
-import { Wrench } from "lucide-react";
+import { Menu, Wrench } from "lucide-react";
 import ThemeToggler from "@/components/ThemeToggler";
 import { useSession, signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
+import TopBar from "./TopBar";
 
 type Props = {};
 
@@ -14,8 +16,13 @@ const handleSignOut = async () => {
 
 export const Header: React.FC = (props: Props) => {
   const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <header className="border bottom-1 w-full">
+      <TopBar isOpen={isOpen} toggle={toggle} session={session} />
       <nav className="border-gray-200 px-4 py-2.5 flex justify-center max-w-[1300px] mx-auto">
         <div className="flex flex-wrap justify-betwen items-center mr-auto max-w-screen-xl">
           <Wrench
@@ -29,10 +36,24 @@ export const Header: React.FC = (props: Props) => {
         </div>
         {session?.user ? (
           <div className="flex items-center gap-4">
-            <Link href="/view-froms">
+            <Button
+              variant="outline"
+              className="hidden max-md:flex mx-2"
+              size="icon"
+            >
+              <Menu
+                className="dark:white light:black self-center"
+                onClick={toggle}
+              />
+            </Button>
+            <Link href="/view-froms" className="hidden md:flex">
               <Button variant="outline">Dashboard</Button>
             </Link>
-            <Button variant="outline" onClick={handleSignOut}>
+            <Button
+              variant="outline"
+              className="hidden md:flex"
+              onClick={handleSignOut}
+            >
               Sign out
             </Button>
             {session.user.name && session.user.image && (
@@ -41,7 +62,7 @@ export const Header: React.FC = (props: Props) => {
                 alt={session.user.name}
                 width={32}
                 height={32}
-                className="rounded-full mr-2"
+                className="hidden md:flex rounded-full mr-2"
               />
             )}
           </div>
