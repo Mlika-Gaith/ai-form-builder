5e02968437632e5cfcbbb8f3b94a774f764ee93d
ghaith <mlika.gaith@gmail.com>
2024-06-15 20:35:23 +0100

    Fix: fix authOptions import in stripe create-portal route

--- src/app/api/stripe/create-portal/route.ts
+++ src/app/api/stripe/create-portal/route.ts
@@ -1,8 +1,8 @@
 "use server";
 import User from "@/db/models/user";
 import { stripe } from "@/lib/stripe";
+import { authOptions } from "@/utils/authOptions";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../auth/[...nextauth]/route";
 
 export async function POST(req: Request) {
   const session = await getServerSession(authOptions);
